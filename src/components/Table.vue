e8b8ccf04d42dbe8cae775a1960d3aec444eccc8
Lesly Revenge <leslyrevenge@Leslys-MacBook-Pro.local>
2020-09-28 11:31:17 -0400

    refactor search

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -3,17 +3,21 @@
     <h1>{{ msg }}</h1>
     <div class="table_container">
       <div class="header_container">
+        <!-- task: show h title and sort values based on such column.
+        purpose: Clicking on a column header that is inactive (i.e. clicking on "Status" when "Row" is active) should cause the clicked method to activate.
+
+         -->
         <div class="header_item" v-on:click="() => onSort('row')">
           Row
-          <i class="  arrow_active " v-bind:class="[dirClass, rowClass]"></i>
+          <i class="arrow_active" v-bind:class="[dirClass, rowClass]"></i>
         </div>
         <div class="header_item" v-on:click="() => onSort('status')">
           Status
-          <i class="  arrow_active " v-bind:class="[dirClass, statusClass]"></i>
+          <i class="arrow_active" v-bind:class="[dirClass, statusClass]"></i>
         </div>
         <div class="header_item" v-on:click="() => onSort('reason')">
           Reason
-          <i class="  arrow_active " v-bind:class="[dirClass, reasonClass]"></i>
+          <i class="arrow_active" v-bind:class="[dirClass, reasonClass]"></i>
         </div>
       </div>
       <div>
@@ -26,6 +30,9 @@
 <script>
 import axios from "axios";
 import Rows from "./Rows";
+// task: import the data via client side in case server is on running.
+// purpose: Attached to this email is a JSON data file for your code to consume.
+// purpose:The array provided in the JSON file will have a number of Successes, Errors and Failures.
 import data from "../data/data.json";
 import { getIncludes, defaultValue, sortValue } from "../mw/search";
 
@@ -35,22 +42,33 @@ export default {
     msg: String,
   },
   methods: {
+    // task: serve data, purpose: backup server
     clientSideServe() {
+      // get the keys that should be displayed and make an array. If no key, default to such array
+      // purpose: For this table, we only want to see Errors and Failures.
       let status = this.show ? this.show.split(",") : ["error", "failure"];
+      // if nomenclature is absent, default based to these values
       let default_dict = {
         status: "Error",
         reason: "Server Error",
       };
+      // get statuses via these callback functions.
       this.statuses = getIncludes(data, status, (v) =>
         defaultValue(v, default_dict, (v) => sortValue(v, this.sort))
       );
     },
+    // task: sort data,
+    //purpose: Only 1 sorting method should ever be active at a time.
+
     onSort(section) {
-      let toggle_direction = this.sort[1] == "asc" ? "desc" : "asc";
+      //purpse: Clicking on a column header that is already active (i.e. clicking on "Row" when "Row" is active) should toggle the direction of the arrow and the sorted data.
+      let toggle_direction = this.sort[1] === "asc" ? "desc" : "asc";
+
       this.sort = [section, toggle_direction];
 
       // if server is available, data will be sorted and sanitized by the server.
       // if not, data will be sorted and sanitized by client.
+      // purpose: fetch new data based on new sort criteriea
 
       axios
         .get(
@@ -66,8 +84,9 @@ export default {
     Rows,
   },
   mounted() {
-    // if server is available, data will be sorted and sanitized by the server.
+    //task: if server is available, data will be sorted and sanitized by the server.
     // if not, data will be sorted and sanitized by client.
+    // purpose: show of knowledge
     axios
       .get(
         `http://localhost:5000/status?show=${this.show}&&sort=${this.sort[0]},${this.sort[1]}`
@@ -80,33 +99,48 @@ export default {
   data: () => {
     return {
       statuses: [],
+      //task: On initial load, your table should default to the "Row" sort,
+      // purpose: Arrows should default to pointing down
       sort: ["row", "asc"],
-      json_status: [],
       show: "error,failure",
     };
   },
   computed: {
+    // task: set the row class, depending on the sort value.
+    // purpose: Clicking on a column header that is inactive (i.e. clicking on "Status" when "Row" is active) should cause the clicked method to activate.
+
     rowClass() {
       return {
-        asc: this.sort[0] == "row" && this.sort[1] !== "asc",
-        desc: this.sort[0] == "row" && this.sort[1] == "asc",
-        arrow_active: this.sort[0] == "row",
+        // task: assign class to get icon.
+        // purpose: Clicking on a column header that is already active (i.e. clicking on "Row" when "Row" is active) should toggle the direction of the arrow and the sorted data.
+        asc: this.sort[0] === "row" && this.sort[1] === "desc",
+        // i prefer commented out code because it provide neutriality
+        // desc: this.sort[0] === "row" && this.sort[1] === "asc",
+        desc: this.sort[0] !== "row" || this.sort[1] !== "desc",
+        // assign class to display icon
+        // pupose: When a particular sorting method is active, the arrow icon associated with that sort should darken. The other arrows should turn gray. In the example image above, the "Row" sort is active, so the arrow next to the "Row" text is black.
+        arrow_active: this.sort[0] === "row",
         arrow_inactive: this.sort[0] !== "row",
       };
     },
     statusClass() {
       return {
-        asc: this.sort[0] == "status" && this.sort[1] !== "asc",
-        desc: this.sort[0] == "status" && this.sort[1] == "asc",
-        arrow_active: this.sort[0] == "status",
+        asc: this.sort[0] === "status" && this.sort[1] === "desc",
+        // i prefer commented out code because it provide neutriality
+        // desc: this.sort[0] === "status" && this.sort[1] === "asc",
+        desc: this.sort[0] !== "status" || this.sort[1] !== "desc",
+        arrow_active: this.sort[0] === "status",
         arrow_inactive: this.sort[0] !== "status",
       };
     },
     reasonClass() {
       return {
-        asc: this.sort[0] == "reason" && this.sort[1] !== "asc",
-        desc: this.sort[0] == "reason" && this.sort[1] == "asc",
-        arrow_active: this.sort[0] == "reason",
+        asc: this.sort[0] === "reason" && this.sort[1] === "desc",
+        // i prefer commented out code because it provide neutriality
+        // desc: this.sort[0] === "reason" && this.sort[1] === "asc",
+        desc: this.sort[0] !== "reason" || this.sort[1] !== "desc",
+
+        arrow_active: this.sort[0] === "reason",
         arrow_inactive: this.sort[0] !== "reason",
       };
     },
@@ -118,25 +152,16 @@ export default {
 .header_container div {
   display: inline-block;
   min-width: 27%;
-  padding: 20px 3%;
+  padding: 10px 3%;
   text-align: left;
 }
 .header_container {
   background-color: #f5f7f9;
 }
 .table_container {
-  width: 800px;
+  width: 600px;
   border: 1px solid #eef1f3;
   border-radius: 3px;
-}
-
-.asc {
-  transform: rotate(-135deg);
-  -webkit-transform: rotate(-135deg);
-}
-.desc {
-  transform: rotate(45deg);
-  -webkit-transform: rotate(45deg);
 }
 
 .header_item {
@@ -145,6 +170,21 @@ export default {
 
 .header_item > i {
   margin-left: 5px;
+}
+/* task : create arrow icons:
+purpose: Arrows should default to pointing down.
+Note: on sorting tables, arrows should show neutral when not in used. 
+either hide the arrow. Thefore, if not active, arrow will be pointed 
+down on a neutral manner. 
+
+ */
+.asc {
+  transform: rotate(-135deg);
+  -webkit-transform: rotate(-135deg);
+}
+.desc {
+  transform: rotate(45deg);
+  -webkit-transform: rotate(45deg);
 }
 
 .arrow_active {
